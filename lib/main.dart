384e84cb0eefd1a78d5f4c45f070c1f12797e244
rahul <rahulsarvaiya3840@gmail.com>
2021-10-08 23:12:32 -0700

    download package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,20 +23,57 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  MyHomePage({Key? key}) : super(key: key);
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  late VideoPlayerController videoPlayerController;
+  late ChewieController chewieController;
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-      ),
-      body: Center(
-        child:Text(""),
-      )
+  void initState() {
+    super.initState();
+    initializeVideoPlayer();
+  }
+
+  Future<void> initializeVideoPlayer() async {
+    videoPlayerController = VideoPlayerController.network(
+        "https://assets.mixkit.co/videos/preview/mixkit-forest-stream-in-the-sunlight-529-large.mp4");
+    await Future.wait([
+      videoPlayerController.initialize()
+    ]);
+    chewieController = ChewieController(
+      videoPlayerController: videoPlayerController,
+      autoPlay: true,
+      looping: true,
     );
   }
-}
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+          appBar: AppBar(
+          ),
+          body: Container(
+            child: Center(
+              child: chewieController != null &&
+                  chewieController.videoPlayerController.value.isInitialized
+                  ? Chewie(
+                controller: chewieController,
+              )
+                  : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircularProgressIndicator(),
+                  SizedBox(height: 20),
+                  Text("Loading"),
+                ],
+              ),
+            ),
+          )
+      );
+    }
+  }
+
